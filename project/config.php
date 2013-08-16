09ee9d0d5e7bc7529e6d3150ccf59e70076632b9
Adam Boerema <adamboerema@gmail.com>
2013-08-16 00:49:26 -0400

    updated config

--- project/config.php
+++ project/config.php
@@ -17,9 +17,10 @@
 	}
 	else{
 		define('DB_HOST', 'localhost');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
-        define('DB_NAME', 'extension'); 
+		define('DB_USER', 'root');
+		define('DB_PASS', 'password!@#$');
+		define('DB_NAME', 'extension'); 
+		
 
 		error_reporting(E_ALL);
 	}
